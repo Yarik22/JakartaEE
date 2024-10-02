a80a614853da483009b9bfd5191cb982b2d37e8e
Yarik22 <popov.yarik.popov@gmail.com>
2024-10-03 00:20:18 +0300

    lr5

---
+++ src/main/java/com/example/myapp/FeedbackContainer.java
@@ -0,0 +1,40 @@
+package com.example.myapp;
+
+import java.io.Serializable;
+
+public class Feedback implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // Конструктор
+    public Feedback(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
